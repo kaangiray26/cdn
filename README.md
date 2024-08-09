b7a6b7b183832427adbf37939474873deb06397e
Kaan Giray Buzluk <kaangiray26@protonmail.com>
2024-08-09 11:47:44 +0200

    page update

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # cdn
 Don't even bother looking here 🙈
+
+## Usage
+List all available files in the CDN:
+```bash
+sh -c "$(curl -fsSL https://cdn.buzl.uk/cdn.sh)" list
+```
+
+Get a file from the CDN:
+```bash
+sh -c "$(curl -fsSL https://cdn.buzl.uk/cdn.sh)" get <file>
+```
---
+++ docs/cdn.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Check if the command line argument is empty
+if [ -z "$1" ]; then
+    echo "Usage: $0 [command]"
+    echo "Commands:"
+    echo "  list        List all files in the cdn"
+    echo "  get [file]  Download a file from the cdn"
+    exit 1
+fi
+
+# Check if the command is list
+if [ "$1" == "list" ]; then
+    curl -s -L \
+    -H "Accept: application/vnd.github+json" \
+    -H "X-GitHub-Api-Version: 2022-11-28" \
+    https://api.github.com/repos/kaangiray26/cdn/contents/docs | python -c "import sys, json; print('\n'.join([f['name'] for f in json.load(sys.stdin)]))"
+fi
+
+# Check if the command is download
+if [ "$1" == "get" ]; then
+    if [ -z "$2" ]; then
+        echo "Usage: $0 get [file]"
+        exit 1
+    fi
+    wget -q https://cdn.buzl.uk/$2
+fi
